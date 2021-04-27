52ac74fcb14a707fa090d52b02135962bf5cce74
Lucas <lucasfcarvalho.17@gmail.com>
2021-04-27 08:25:06 -0300

    Java

---
+++ Java/workspace/aulaJava/src/programa/BalancoAnual.java
@@ -0,0 +1,16 @@
+package programa;
+
+import java.util.*;
+
+
+
+public class BalancoAnual {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner  mes = new Scanner(System.in);
+		
+		int gastosJaneiro = 15000, gastosFevereiro = 23000 ,gastosMarco = 17000;
+		int gastoAbriu = 10000, gastoMaio
+
+}
